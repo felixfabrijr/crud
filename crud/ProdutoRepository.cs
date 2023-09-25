ee3fdbe07d4ac2fbdbfa0ce6cb0497b6caa5710c
FelixFabri <felix.fabrijr@gmail.com>
2023-09-25 19:51:11 +0100

    Add project files.

---
+++ crud/ProdutoRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ProdutoRepository
+{
+    private List<Produto> produtos = new List<Produto>();
+    private int proximoId = 1;
+
+    // Create
+    public void Adicionar(Produto produto)
+    {
+        produto.Id = proximoId++;
+        produtos.Add(produto);
+    }
+
+    // Read (Obter todos os produtos)
+    public List<Produto> ObterTodos()
+    {
+        return produtos;
+    }
+
+    // Read (Obter um produto por ID)
+    public Produto ObterPorId(int id)
+    {
+        return produtos.FirstOrDefault(p => p.Id == id);
+    }
+
+    // Update
+    public void Atualizar(Produto produtoAtualizado)
+    {
+        var produtoExistente = produtos.FirstOrDefault(p => p.Id == produtoAtualizado.Id);
+        if (produtoExistente != null)
+        {
+            produtoExistente.Nome = produtoAtualizado.Nome;
+            produtoExistente.Preco = produtoAtualizado.Preco;
+        }
+    }
+
+    // Delete
+    public void Remover(int id)
+    {
+        var produtoExistente = produtos.FirstOrDefault(p => p.Id == id);
+        if (produtoExistente != null)
+        {
+            produtos.Remove(produtoExistente);
+        }
+    }
+}
---
+++ crud/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var produtoRepository = new ProdutoRepository();
+
+        // Adicionar um produto
+        produtoRepository.Adicionar(new Produto { Nome = "Produto 1", Preco = 19.99m });
+
+        // Obter todos os produtos
+        var produtos = produtoRepository.ObterTodos();
+        Console.WriteLine("Produtos:");
+
+        foreach (var produto in produtos)
+        {
+            Console.WriteLine($"ID: {produto.Id}, Nome: {produto.Nome}, Preço: {produto.Preco:C}");
+        }
+
+        // Atualizar um produto
+        var produtoParaAtualizar = produtoRepository.ObterPorId(1);
+        if (produtoParaAtualizar != null)
+        {
+            produtoParaAtualizar.Nome = "Produto Atualizado";
+            produtoParaAtualizar.Preco = 29.99m;
+            produtoRepository.Atualizar(produtoParaAtualizar);
+            Console.WriteLine("Produto atualizado.");
+        }
+
+        // Remover um produto
+        produtoRepository.Remover(1);
+        Console.WriteLine("Produto removido.");
+
+        Console.ReadLine();
+    }
+}
+
